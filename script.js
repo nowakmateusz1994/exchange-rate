8ced8c466d45140bab8ded4315f5101dd096c81d
nowak-mateusz1994 <34008481+nowak-mateusz1994@users.noreply.github.com>
2020-02-24 19:16:21 +0100

    wyświetlenie wszystkich danych w pliku HTML
    wyświetlenie pobranych danych na stronie www

--- script.js
+++ script.js
@@ -1,17 +1,38 @@
 let api = 'http://api.nbp.pl/api/exchangerates/tables/c/'
 let table = document.querySelector('.table');
 
-function showTable(table){
- let tableArray = table[0].rates;
- let date = table[0].effectiveDate; 
- console.log(date)
+function creatDivWithClass(value, ...className) {
+    let div = document.createElement('div');
+    className.forEach(element => {
+        div.classList.add(element);
+    });
+    div.innerText = value;
+    table.appendChild(div);
+}
+
+function showTable(rate) {
+    let tableArray = rate[0].rates;
+    let date = rate[0].effectiveDate;
+    creatDivWithClass(date, "header");
+    creatDivWithClass('Nazwa Waluty', "currency", 'title');
+    creatDivWithClass('Kod', "code", 'title');
+    creatDivWithClass('Kupno', "bid", 'title');
+    creatDivWithClass('Sprzedaż', "ask", 'title');
+
+    tableArray.forEach(element => {
+        creatDivWithClass(element.currency, "currency");
+        creatDivWithClass(element.code, "code");
+        creatDivWithClass(element.bid, "bid");
+        creatDivWithClass(element.ask , "ask");
+    });
+
 }
 
 
-function getCurrentRate(api){
+function getCurrentRate(api) {
     fetch(api)
-    .then(rate => rate.json())
-    .then(rate => showTable(rate));
+        .then(rate => rate.json())
+        .then(rate => showTable(rate));
 }
 
 getCurrentRate(api);
