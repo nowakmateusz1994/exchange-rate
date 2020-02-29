12f824b0f2c06caeda0c2950c22d36b2af9a4ec7
nowak-mateusz1994 <34008481+nowak-mateusz1994@users.noreply.github.com>
2020-02-29 21:20:47 +0100

    prosty przelicznik różnych walut na PLN

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
 let api = 'http://api.nbp.pl/api/exchangerates/tables/c/'
 let table = document.querySelector('.table');
+let date = null;
+let tableArray = null;
+
+let currencyList = document.querySelector('.converter .cod');
+let converterSubmit = document.querySelector('button.submit');
+let converterOption = document.querySelector('select.option');
+let converterInput = document.querySelector('.converter input');
+let converterInfo = document.querySelector('.converter span')
+
+function addOptions(optionsArray) {
+
+    optionsArray.forEach((element, index) => {
+        let option = document.createElement('option');
+        option.innerText = element.code;
+        option.value = index;
+        currencyList.appendChild(option);
+    });
+}
+
+function converterCurrency() {
+    if (converterOption.value == 'bouy') {
+        converterInfo.innerText =`Aby kupić ${converterInput.value} ${tableArray[currencyList.value].code} potrzebujesz  ${(tableArray[currencyList.value].ask*converterInput.value).toFixed(2)} PLN `; 
+    } else if (converterOption.value == 'sell') {
+        converterInfo.innerText =`Za ${converterInput.value} ${tableArray[currencyList.value].code} dostaniesz ${(tableArray[currencyList.value].bid*converterInput.value).toFixed(2)} PLN`;
+    }
+}
 
 function creatDivWithClass(parent, value, ...className) {
     let div = document.createElement('div');
@@ -12,23 +38,24 @@ function creatDivWithClass(parent, value, ...className) {
 }
 
 function showTable(rate) {
-    let tableArray = rate[0].rates;
-    let date = rate[0].effectiveDate;
-    creatDivWithClass(table, date ,"header");
+    tableArray = rate[0].rates;
+    date = rate[0].effectiveDate;
+    creatDivWithClass(table, date, "header");
     let wrap = creatDivWithClass(table, null, "wrap");
-    creatDivWithClass(wrap,'Nazwa Waluty', "currency", 'title');
+    creatDivWithClass(wrap, 'Nazwa Waluty', "currency", 'title');
     creatDivWithClass(wrap, 'Kod', "code", 'title');
-    creatDivWithClass(wrap,'Kupno', "bid", 'title');
-    creatDivWithClass(wrap,'Sprzedaż', "ask", 'title');
+    creatDivWithClass(wrap, 'Kupno', "bid", 'title');
+    creatDivWithClass(wrap, 'Sprzedaż', "ask", 'title');
 
     tableArray.forEach(element => {
         let parent = creatDivWithClass(table, null, "wrap");
         creatDivWithClass(parent, element.currency, "currency");
         creatDivWithClass(parent, element.code, "code");
         creatDivWithClass(parent, element.bid, "bid");
-        creatDivWithClass(parent, element.ask , "ask");
+        creatDivWithClass(parent, element.ask, "ask");
     });
 
+    addOptions(tableArray);
 }
 
 
@@ -38,4 +65,6 @@ function getCurrentRate(api) {
         .then(rate => showTable(rate));
 }
 
+
 getCurrentRate(api);
+converterSubmit.addEventListener('click', converterCurrency)
